e927c379688302dad86a4ff65b4c674d3ce94d34
wchunocca-insn <insn.soporte.ads@gmail.com>
2023-07-23 15:45:14 -0500

    Subir script
    Todo OK

---
+++ scripts/cvs_delete_n5.sql
@@ -0,0 +1,26 @@
+
+select count(*) from eco_tempo_N1 with(nolock)
+select count(*) from eco_tempo_N2 with(nolock)
+select count(*) from eco_tempo_N3 with(nolock)
+select count(*) from eco_tempo_N4 with(nolock)
+select count(*) from eco_tempo_N5 with(nolock)
+
+
+
+select * from eco_tempo_N1 with(nolock)
+select * from eco_tempo_N2 with(nolock)
+select * from eco_tempo_N3 with(nolock)
+select * from eco_tempo_N4 with(nolock)
+select * from eco_tempo_N5 with(nolock)
+
+/*
+
+truncate table eco_tempo_N1
+truncate table eco_tempo_N2
+truncate table eco_tempo_N3
+truncate table eco_tempo_N4
+truncate table eco_tempo_N5
+
+*/
+
+--select top 10 * from eco_tempoTarea
